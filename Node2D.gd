b18b180975916bc1e269df0f7370380a57276c3b
João Pedro Rios Carvalho <jprcarvalho1@gmail.com>
2024-04-16 01:33:41 -0300

    v2

--- Node2D.gd
+++ Node2D.gd
@@ -4,7 +4,7 @@ extends Node2D
 @export var enemy:RigidBody2D
 @export var base:StaticBody2D
 @export var base_inimiga:StaticBody2D
-var gold_cost = [50,100,80,300,200]
+var gold_cost = [50, 100, 80, 300, 200]
 var gold = 100
 var gold_gain = 12
 var CD = .9
@@ -12,9 +12,9 @@ var cd = .9
 var cd_u = .9
 var POS_Y 
 var POS_X 
-var buttons = [false,false,false,false,false]
+var buttons = [false, false, false, false, false]
 var start = false
-var cd_upgrades = [99E10,99E10,99E10,99E10,99E10]
+var cd_upgrades = [99E10, 99E10, 99E10, 99E10, 99E10]
 var spawn_time = 15
 var spawn_enamy_timer = 0
 var enamy_upgrades_timer = 0
@@ -27,34 +27,36 @@ var win_text = [
 	'Parabéns por dominar senoidais.\n Funções senoidais são um tipo especial de função com um comportamento \"repetitivo\", ou seja, ela é feita for infinitas repetições de um pedaço.',
 ]
 var aux_level = 0
+var dialog
 
-func set_up(b:int,life_in:int):
+func set_up(b:int, life_in:int):
 	level = b
-	gold = b*gold
+	gold = b * gold
 	var hbox = get_node("Camera/HBoxContainer")
-	if 0<b and b<6:
+	if 0 < b and b < 6:
 		for a in range(b):
-			cd_upgrades[a] =(randi()%9 + 1)*(level/4)+10
-			hbox.get_child(a).visible= true
+			cd_upgrades[a] = (randi() % 9 + 1) * (level / 4) + 10
+			hbox.get_child(a).visible = true
 			buttons[a] = true
-		for a in range(b,5):
-			hbox.get_child(a).visible= false
+		for a in range(b, 5):
+			hbox.get_child(a).visible = false
 			buttons[a] = false
 	get_node('Camera').visible = true
-	base.setup(1,life_in)
-	base_inimiga.setup(-1,life_in)
+	base.setup(1, life_in)
+	base_inimiga.setup(-1, life_in)
 	start = true
 
 func _ready():
 	get_node("Camera").visible = false
 	POS_Y = 390
-	POS_X = [144,1030]
+	POS_X = [144, 1030]
+	dialog = get_node("Window")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if start:
-		enamy_upgrades_timer+= delta
-		spawn_enamy_timer+= delta
+		enamy_upgrades_timer += delta
+		spawn_enamy_timer += delta
 		gold += gold_gain * delta
 		get_node("Camera/Label").text = str(round(gold))
 		#var po = get_node("Camera/Label")
@@ -62,23 +64,23 @@ func _process(delta):
 			#po.text+='\n'+str(cd_upgrades[a])
 		#po.text+='\n'+str(enamy_upgrades_timer)
 		#po.text += '\n'+str(spawn_time-spawn_enamy_timer)
-		cd+=delta
-		cd_u+=delta
-		if(Input.is_key_pressed(KEY_S) and gold>100 and cd>CD):
-			gold -=100
+		cd += delta
+		cd_u += delta
+		if(Input.is_key_pressed(KEY_S) and gold > 100 and cd > CD):
+			gold -= 100
 			create(1,base.power.duplicate())
-			cd=0
+			cd = 0
 		if spawn_enamy_timer > spawn_time:
 			var __power = base_inimiga.power.duplicate()
-			create(-1,__power,gold_gain_on_death(__power))
-			spawn_enamy_timer-=spawn_time
+			create(-1, __power, gold_gain_on_death(__power))
+			spawn_enamy_timer -= spawn_time
 			if level == 2:
 				if spawn_time > 5:
-					spawn_time-=.4
+					spawn_time -= .4
 			elif level == 3:
-				spawn_time = 10+ randi()%10
+				spawn_time = 10 + randi() % 10
 			elif level == 4:
-				spawn_time = (10-aux_level)+ randi()%10
+				spawn_time = (10 - aux_level) + randi() % 10
 				aux_level += 1
 				if aux_level == 8:
 					aux_level = 0
@@ -90,69 +92,57 @@ func _process(delta):
 				elif aux_level == 9:
 					spawn_time = 1
 				else:
-					spawn_time = 10+ randi()%10
+					spawn_time = 10 + randi() % 10
 		for a in range(5):
 			if enamy_upgrades_timer > cd_upgrades[4-a]:
-				enamy_upgrades_timer=0
-				base_inimiga.update(4-a,1)
-				cd_upgrades[4-a] = (randi()%9 + 1)*(level/2)+10
+				enamy_upgrades_timer = 0
+				base_inimiga.update(4 - a, 1)
+				cd_upgrades[4-a] = (randi() % 9 + 1) * (level / 2) + 10
 				for t in range(5):
-					if t!=a:
-						cd_upgrades[4-t]-=1
+					if t != a:
+						cd_upgrades[4-t] -= 1
 				#print(cd_upgrades)
-		if(Input.is_key_pressed(KEY_1) and buttons[0] and cd_u>CD):
+		if(Input.is_key_pressed(KEY_1) and buttons[0] and cd_u > CD):
 			_on_const_bnt_pressed()
-			cd_u=0
-		if(Input.is_key_pressed(KEY_2) and buttons[1] and cd_u>CD):
+			cd_u = 0
+		if(Input.is_key_pressed(KEY_2) and buttons[1] and cd_u > CD):
 			_on_lin_btn_2_pressed()
-			cd_u=0
-		if(Input.is_key_pressed(KEY_3) and buttons[2] and cd_u>CD):
+			cd_u = 0
+		if(Input.is_key_pressed(KEY_3) and buttons[2] and cd_u > CD):
 			_on_log_btn_3_pressed()
-			cd_u=0
-		if(Input.is_key_pressed(KEY_4) and buttons[3] and cd_u>CD):
+			cd_u = 0
+		if(Input.is_key_pressed(KEY_4) and buttons[3] and cd_u > CD):
 			_on_exp_btn_4_pressed()
-			cd_u=0
-		if(Input.is_key_pressed(KEY_5) and buttons[4] and cd_u>CD):
+			cd_u = 0
+		if(Input.is_key_pressed(KEY_5) and buttons[4] and cd_u > CD):
 			_on_sin_btn_5_pressed()
-			cd_u=0
+			cd_u = 0
 		if base.life <= 0:
+			dialog.find_child("Label").text = 'Tente Novamente.\n\n Estude um pouco mais para dominar as funções matemáticas \n\n e derrotar os orcs.'
+			dialog.visible = true
 			print('lose')
 			start = false
 		if base_inimiga.life <= 0:
+			dialog.find_child("Label").text = win_text[level-1]
+			dialog.visible = true
 			print('win')
 			start = false
-			var bg = ColorRect.new()
-			bg.color= Color(0,0,0,.7)
-			bg.size = Vector2(get_viewport_rect().size.x*1.1, get_viewport_rect().size.y*1.1)
-			add_child(bg)
-			var label = Label.new()
-			label.text = win_text[level-1]
-			bg.add_child(label)
-			label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-			label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
-			var btn = Button.new()
-			btn.position = Vector2(get_viewport_rect().size.x/2, get_viewport_rect().size.y*.9)
-			btn.text= 'Selecionar \n Level'
-			add_child(btn)
-			btn.connect("pressed", _on_button_pressed)
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://level selection.tscn")
-			
-			
-func create(team,power,gold=null):
+
+func create(team, power, gold = null):
 	var b;
 	b = buneco.duplicate()
 	b.DUPLICATE_SCRIPTS
-	b.setup(team,power,Vector2(POS_X[ (team-1)/2 *-1 ],POS_Y),gold)
+	b.setup(team,power,Vector2(POS_X[(team - 1) / 2 * -1], POS_Y), gold)
 	add_child(b)
 		#print(b,'  team ',team)
-
 
 func gold_gain_on_death(p):
 	var g = 0
 	for n in range(5):
-		g += p[n]*gold_cost[n]/10
+		g += p[n] * gold_cost[n] / 10
 	return g
 
 func gain_gold(g):
@@ -176,4 +166,4 @@ func _on_sin_btn_5_pressed():
 func update(n):
 	if gold > gold_cost[n]:
 		gold -= gold_cost[n]
-		base.update(n,1)
+		base.update(n, 1)
